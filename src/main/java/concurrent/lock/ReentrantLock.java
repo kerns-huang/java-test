f903c3cadc675971e14ab131d6cd0494833d07a1
kk <realmall20@protonmail.com>
2020-07-26 19:24:24 +0800

    m

--- src/main/java/concurrent/lock/ReentrantLock.java
+++ src/main/java/concurrent/lock/ReentrantLock.java
@@ -24,7 +24,7 @@ public class ReentrantLock implements Lock {
      */
     private AtomicInteger counter;
 
-    private LinkedBlockingQueue waitQueue;
+    private LinkedBlockingQueue<Thread> waitQueue;
 
     public ReentrantLock() {
         waitQueue = new LinkedBlockingQueue(100);
@@ -34,20 +34,24 @@ public class ReentrantLock implements Lock {
      * 一直尝试获取锁
      */
     public void lock() {
-        int count = counter.get();
-        if (count > 0) {
-            if (owner.get() == Thread.currentThread()) {
-                //拥有人是当前当前线程
-                counter.compareAndSet(count, count + 1);
-            } else {
-                //把当前线程放到等待队列里面
-                waitQueue.offer(Thread.currentThread());
-                LockSupport.park();
-            }
-        } else {
-            if (counter.compareAndSet(0, 1)) {
-                //如果当前线程能够设置成1，设置所有人为当前现线程。
-                owner.set(Thread.currentThread());
+        if (!tryLock()) {
+            //如果获取不到锁，放入到等待队列
+            waitQueue.offer(Thread.currentThread());
+            while (true) {
+                //取头部数据
+                Thread head = waitQueue.peek();
+                if (head == Thread.currentThread()) {
+                    if (!tryLock()) {
+                        //如果获取不到锁，说明是其它线程还占有的锁。挂起之后什么时候唤醒了，unlock的时候唤醒？
+                        LockSupport.park();
+                    } else {
+                        //获取到了锁，直接弹出当前线程。
+                        waitQueue.poll();
+                        return;
+                    }
+                } else {
+                    LockSupport.park();
+                }
             }
         }
 
@@ -74,17 +78,17 @@ public class ReentrantLock implements Lock {
             if (owner.get() == Thread.currentThread()) {
                 //拥有人是当前当前线程
                 counter.compareAndSet(count, count + 1);
-                return  true;
+                return true;
             } else {
                 //把当前线程放到等待队列里面
-               return false;
+                return false;
             }
         } else {
-            if (counter.compareAndSet(0, 1)) {
+            if (counter.compareAndSet(count, count + 1)) {
                 //如果当前线程能够设置成1，设置所有人为当前现线程。
                 owner.set(Thread.currentThread());
                 return true;
-            }else {
+            } else {
                 //被其它的线程抢占，设置了，直接返回false
                 return false;
             }
@@ -100,7 +104,15 @@ public class ReentrantLock implements Lock {
      * @throws InterruptedException
      */
     public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        return false;
+        if(owner.get()!=Thread.currentThread()){
+            //TODO 抛出异常
+            throw new IllegalMonitorStateException("当前线程不是执行线程");
+        }
+        int count=    counter.decrementAndGet();
+        if(count<=0){
+            owner.set(null);
+        }
+        return true;
     }
 
     /**
@@ -114,12 +126,5 @@ public class ReentrantLock implements Lock {
         return null;
     }
 
-    abstract static class Sync extends AbstractQueuedSynchronizer {
-        /**
-         * 当前锁拥有人
-         */
-        private volatile Thread owner;
 
-
-    }
 }
